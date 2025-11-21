d0f505566e172a765f72db3bf1ef1e427a4bcc36
Shuntoria Reid <shuntoriareid240@gmail.com>
2025-11-21 06:11:24 -0500

    update

--- js/index.js
+++ js/index.js
@@ -9,16 +9,16 @@ fetch(url)
   })
   .then((data) => {
     console.log("Marvel API response:", data);
+
+    const firstCharacter = data.data && data.data.results && data.data.results[0];
+    if (firstCharacter) {
+      console.log("Name:", firstCharacter.name);
+      console.log("Description:", firstCharacter.description);
+    } else {
+      console.log("No characters found in response.");
+    }
   })
   .catch((error) => {
     console.error("Error fetching Marvel data:", error);
   });
-
-  .then((data) => {
-  console.log("Marvel API response:", data);
-
-  const firstCharacter = data.data.results[0];
-  console.log("Name:", firstCharacter.name);
-  console.log("Description:", firstCharacter.description);
-})
 
